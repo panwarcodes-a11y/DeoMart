8fddf4c695f51c30a968514a8ba56934d6bfbc93
panwarcodes-a11y <panwarcodes@gmail.com>
2026-05-21 12:10:23 +0530

    Initialize Firebase and implement authentication

--- firebase.js
+++ firebase.js
@@ -1,1 +1,67 @@
-const firebaseConfig = {};
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+getAuth,
+GoogleAuthProvider,
+signInWithPopup,
+signOut,
+onAuthStateChanged
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+getFirestore,
+doc,
+setDoc
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+apiKey: "YOUR_API_KEY",
+authDomain: "deomart-bf41e.firebaseapp.com",
+projectId: "deomart-bf41e",
+storageBucket: "deomart-bf41e.appspot.com",
+messagingSenderId: "690880758075",
+appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
+
+// LOGIN
+window.login = async ()=>{
+const result = await signInWithPopup(auth,provider);
+const user = result.user;
+
+await setDoc(doc(db,"users",user.uid),{
+name:user.displayName,
+email:user.email,
+photo:user.photoURL
+});
+
+alert("Welcome " + user.displayName);
+}
+
+// LOGOUT
+window.logout = ()=>signOut(auth);
+
+// UI
+onAuthStateChanged(auth,(user)=>{
+
+let box = document.getElementById("user-box");
+
+if(user){
+box.innerHTML = `
+<img src="${user.photoURL}" width="50">
+<p>${user.displayName}</p>
+<button onclick="logout()">Logout</button>
+`;
+}else{
+box.innerHTML = `
+<button onclick="login()">Login With Google</button>
+`;
+}
+
+});
